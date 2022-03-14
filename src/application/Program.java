0eabce3d211b8ef29fb718c14a224c1a8dfa32ba
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-03-14 15:58:57 -0300

    Feat: Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Department obj = new Department(1, " Clothes and Shoes");
+		System.out.println(obj);
+
+	} 
+
+}
