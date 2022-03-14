5723c6723c45fd07f71a4b94dd4083ce6713b1e3
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-03-14 16:23:30 -0300

    Feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		Department obj = new Department(1, " Clothes and Shoes");
-		System.out.println(obj);
 
-	} 
+		Department obj = new Department(1, " Clothes and Shoes");
+		Seller seller = new Seller(1, "LuanLucas", "luanlucas.r10@hotmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
+
+	}
 
 }
