96a2d8d2a61f2c7f0a7ba4ef22fd65478a82f0cf
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-03-15 09:51:15 -0300

    Feat: findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,14 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, " Clothes and Shoes");
-		Seller seller = new Seller(1, "LuanLucas", "luanlucas.r10@hotmail.com", new Date(), 3000.0, obj);
+		SellerDao  sellerDao = DaoFactory.createSellerDao();
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
-
+		
+		
+		
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -3,116 +3,102 @@ package model.entities;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
+import model.entities.Department;
 
 public class Seller implements Serializable {
-		
+
 	private static final long serialVersionUID = 1L;
-		private Integer id;
-		private String name;
-		private String email;
-		private Date birthDate;
-		private Double baseSalary;
-		
-		
-		private Department department;
-		
-		public Seller () {
-			
-		}
-		
-		public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
-			
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.birthDate = birthDate;
-			this.baseSalary = baseSalary;
-			this.department = department;
-		}
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
 
+	private Department department;
 
+	public Seller() {
 
+	}
 
-		public Integer getId() {
-			return id;
-		}
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-		public String getName() {
-			return name;
-		}
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public String getEmail() {
+		return email;
+	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		public String getEmail() {
-			return email;
-		}
+	public Date getBirthDate() {
+		return birthDate;
+	}
 
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
 
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
 
-		public Date getBirthDate() {
-			return birthDate;
-		}
+	public Department getDepartment() {
+		return department;
+	}
 
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 
-		public void setBirthDate(Date birthDate) {
-			this.birthDate = birthDate;
-		}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
 
-		public Double getBaseSalary() {
-			return baseSalary;
-		}
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + "]";
+	}
 
-
-		public void setBaseSalary(Double baseSalary) {
-			this.baseSalary = baseSalary;
-		}
-
-
-		
-
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(id);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Seller other = (Seller) obj;
-			return Objects.equals(id, other.id);
-		}
-
-		@Override
-		public String toString() {
-			return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate
-					+ ", baseSalary=" + baseSalary + "]";
-		}
-		
-		
-	 
-		
-		
-	
-	
 }
