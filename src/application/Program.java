c45d8c921dc52ed239f8d6645c00653482237d1d
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-03-15 08:41:37 -0300

    Feat: SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 
 		Department obj = new Department(1, " Clothes and Shoes");
 		Seller seller = new Seller(1, "LuanLucas", "luanlucas.r10@hotmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 	}
